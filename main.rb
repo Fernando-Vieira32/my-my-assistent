12c00ad3e24d9b0d1bd5897b7f40b95e0520a8cc
Fernando Vieira <fernandovieira.32@hotmail.com>
2026-01-03 19:54:51 -0300

    feat(main) ajust spec

--- main.rb
+++ main.rb
@@ -3,8 +3,6 @@
 require_relative 'chat_core'
 
 class Main
-
-
   def self.call(...) = new(...).call
 
   def initialize(model: 'qwen2.5:7b', host: 'http://localhost:11434')
@@ -14,24 +12,8 @@ class Main
 
   def call
     puts "Digite 'sair' ou 'exit' para encerrar\n\n"
-
-    loop do
-      print 'Você: '
-      input = gets.chomp
-
-      break if %w[sair exit].include?(input.downcase)
-
-      next if input.strip.empty?
-
-      print 'my-assistent: '
-      response = chat_core.call(input, history: )
-      puts response
-      puts "\n"
-
-      add_to_history('user', input)
-      add_to_history('assistant', response)
-    end
-
+    chat_loop
+    show_goodbye
     puts 'Até logo!'
   end
 
@@ -39,11 +21,49 @@ class Main
 
   private
 
+  def chat_loop
+    loop do
+      input = read_user_input
+      break if exit_command?(input)
+      next if input.strip.empty?
+
+      process_message(input)
+    end
+  end
+
+  def read_user_input
+    print 'Você: '
+    gets.chomp
+  end
+
+  def exit_command?(input)
+    %w[sair exit].include?(input.downcase)
+  end
+
+  def process_message(input)
+    response = get_ai_response(input)
+    display_response(response)
+    save_conversation(input, response)
+  end
+
+  def get_ai_response(input)
+    chat_core.call(input, history:)
+  end
+
+  def display_response(response)
+    print 'my-assistent: '
+    puts response
+    puts "\n"
+  end
+
+  def save_conversation(input, response)
+    add_to_history('user', input)
+    add_to_history('assistant', response)
+  end
+
   def add_to_history(role, content)
     @history << { role: role, content: content }
   end
 end
 
-if __FILE__ == $PROGRAM_NAME
-  Main.call
-end
+Main.call if __FILE__ == $PROGRAM_NAME
